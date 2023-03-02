aa93d716270ea277b1a566e974df5531e187e65e
Игорь Родин <igor-rodin@iMac-Igor.local>
2023-03-02 20:52:19 +0300

    Add Counter with try_with_resources

--- src/java/com/rodini/kennel/view/View.java
+++ src/java/com/rodini/kennel/view/View.java
@@ -1,2 +1,27 @@
-package com.rodini.kennel.view;public interface View {
+package com.rodini.kennel.view;
+
+import com.rodini.kennel.model.AbstractAnimal;
+
+public interface View {
+    enum MainMenuCommand {
+        ADD_ANIMAL,
+        SHOW_ANIMAL,
+        REMOVE_ANIMAL,
+        EXIT
+    }
+
+    enum AddSkillMenuCommand {
+        ADD_SKILL,
+        EXIT
+    }
+    void showKennelRegistry();
+
+    MainMenuCommand showMainMenuWithResult();
+
+    boolean showAddAnimalDialog();
+    int showRemoveAnimalDialog();
+    void showDetailInfoAnimal();
+    void showAnimalInfo(AbstractAnimal animal);
+    AddSkillMenuCommand showAddSkillMenu(AbstractAnimal animal);
+    boolean showAddSkillDialog(AbstractAnimal animal);
 }
